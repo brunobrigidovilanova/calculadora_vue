5c8be83bedd7561fe5a133a55683599f8b750b37
Bruno Brigido Vilanova <brunobrigidovilanova@gmail.com>
2023-06-29 08:56:33 -0300

    finalizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, watch } from 'vue';
+
+const estado = reactive({
+    calculo1: '',
+    calculo2: '',
+    operacao: 'soma',
+    resultado: 0
+});
+
+const calcular = () => {
+    const numero1 = parseFloat(estado.calculo1);
+    const numero2 = parseFloat(estado.calculo2);
+
+    if (!isNaN(numero1) && !isNaN(numero2)) {
+        switch (estado.operacao) {
+            case 'soma':
+                estado.resultado = numero1 + numero2;
+                break;
+            case 'subtracao':
+                estado.resultado = numero1 - numero2;
+                break;
+            case 'multiplicacao':
+                estado.resultado = numero1 * numero2;
+                break;
+            case 'divisao':
+                estado.resultado = numero1 / numero2;
+                break;
+        }
+    } else {
+        estado.resultado = 0;
+    }
+};
+
+watch(
+    () => [estado.calculo1, estado.calculo2, estado.operacao],
+    () => calcular(),
+    { immediate: true }
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+            <h1>Calculadora aritmética</h1>
+        </header>
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input @input="calculate" v-model="estado.calculo1" type="number" placeholder="Número 1"
+                        class="form-control">
+                </div>
+                <div class="col-md-1">
+                    <select @input="calculate" v-model="estado.operacao" class="form-control select">
+                        <option value="soma">+</option>
+                        <option value="subtracao">-</option>
+                        <option value="multiplicacao">x</option>
+                        <option value="divisao">/</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <input @input="calculate" v-model="estado.calculo2" type="number" placeholder="Número 2"
+                        class="form-control">
+                </div>
+            </div>
+        </form>
+        <p class="p-2 mt-2 bg-light rounded-3">
+            O resultado é {{ estado.resultado }}
+        </p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.select {
+    color: #fff;
+    background-color: rgb(0, 119, 255);
+    font-size: large;
+    text-align: center;
 }
 </style>
